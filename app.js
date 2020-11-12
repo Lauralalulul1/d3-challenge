27084abf94bd9c4534eab3efe2e2e4febc8eb1da
Lauralalulul1 <68302698+Lauralalulul1@users.noreply.github.com>
2020-11-11 21:22:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 // @TODO: YOUR CODE HERE!
 // @TODO: YOUR CODE HERE!
+// source solution 12 16.3 
 var svgWidth = 960;
-var svgHeight = 700;
+var svgHeight = 500;
+
 
 var margin = {
   top: 20,
   right: 40,
-  bottom: 60,
+  bottom: 80,
   left: 100
 };
+
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("#scatter")
+var svg = d3
+  .select(".scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+// Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 
 d3.csv("assets/data/data.csv").then(function (stateData) {
   console.log(stateData);
@@ -30,12 +36,15 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
     });
-
-  // Step 2: Create scale functions
-  // ==============================
-  var xLinearScale = d3.scaleLinear()
+ // create scales
+   var xLinearScale = d3.scaleLinear()
     .domain([0, d3.max(stateData, d => d.poverty)])
     .range([0, width]);
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
 
   var yLinearScale = d3.scaleLinear()
     .domain([0, d3.max(stateData, d => d.healthcare)])
@@ -55,8 +64,7 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
   chartGroup.append("g")
     .call(leftAxis);
 
-  // Step 5: Create Circles
-  // // ==============================
+ // create the circles
   var circlesGroup = chartGroup.selectAll("circle")
     .data(stateData)
     .enter()
@@ -70,8 +78,7 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
 
     .attr("opacity", ".5");  
 
-  // Step 6: Initialize tool tip
-  // ==============================
+ // function used for updating circles group with new tooltip
   var toolTip = d3.tip()
     .attr("class", "tooltip")
     .offset([80, -60])
@@ -79,8 +86,7 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
       return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
     });
 
-  // Step 7: Create tooltip in the chart
-  // ==============================
+  
   chartGroup.call(toolTip);
 
   // Step 8: Create event listeners to display and hide the tooltip
